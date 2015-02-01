192bc5212d0fb0b9ec68f7c311f3196791ac13f9
Marco Pfeiffer <marco@hauptsache.net>
2015-02-01 03:18:22 +0100

    added fastclick

--- opt-in.js
+++ opt-in.js
@@ -3,11 +3,21 @@
 
     var $document = $(document);
 
+    $document.on('ready', function (e) {
+        if (typeof FastClick !== 'undefined') {
+            FastClick.attach(document.body);
+        }
+    });
+
     $document.on('ready update', function (e) {
         var $target = $(e.target);
 
-        $target.find('[data-toggle=tooltip]').tooltip();
-        $target.find('[data-toggle=popover]').popover();
+        if ($.isFunction($.fn.tooltip)) {
+            $target.find('[data-toggle=tooltip]').tooltip();
+        }
+        if ($.isFunction($.fn.popover)) {
+            $target.find('[data-toggle=popover]').popover();
+        }
     });
 
 })(jQuery);
